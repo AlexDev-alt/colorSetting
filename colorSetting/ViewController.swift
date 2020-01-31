ded41a57dea3f4d6ea412b9d3d8b3a43a3ae69f3
AlexDev <aa.devidze@gmail.com>
2020-01-31 17:27:24 +0200

    Initial Commit

---
+++ colorSetting/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  colorSetting
+//
+//  Created by Macbook on 31.01.2020.
+//  Copyright © 2020 Alex Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var collorView: UIView!
+    
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    
+    @IBAction func redSliderAction(_ sender: Any) {
+    }
+    
+    @IBAction func greenSliderAction(_ sender: Any) {
+    }
+    
+    @IBAction func blueSliderAction(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+
+
+}
+
